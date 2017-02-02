85d28f6860b17ad96a6cfdf6a10b7c6dac3c63e9
henrique <henrique@192.168.250.122>
2017-02-02 17:45:56 +0000

    public strings

--- TrOjAn-Infetado/src/trojan/infetado/Main.java
+++ TrOjAn-Infetado/src/trojan/infetado/Main.java
@@ -1,12 +1,25 @@
 package trojan.infetado;
 
 import java.awt.AWTException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import network.*;
 
 public class Main {
+    
+    PrintWriter outt;
+    BufferedReader inn;
+    InputStream in = null;
+    OutputStream out = null;
+    Socket socket = null;
 
     public static void main(String[] args) throws AWTException, IOException {
 
